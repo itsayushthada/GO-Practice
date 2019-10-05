b64c387138adf7e5200ba5e73ef3e22032232cd4
itsayushthada <itsayushthada@gmail.com>
2019-10-05 22:15:23 +0530

    Updated Findian

--- Week2/Findian/src/Findian.go
+++ Week2/Findian/src/Findian.go
@@ -1,29 +1,42 @@
 package main
 
 import (
-	"bufio"
 	"strings"
 	"fmt"
-	"os"
 )
+
+func IsPattern1(data string) bool{
+	input := []rune(data)
+	var length int = len(input);
+	
+	if (input[0] != 'i'|| input[length-1] != 'n'){
+		return false
+	}
+	
+	for i:=1; i<length-1; i++{
+		if input[i] == 'a'{
+			return true
+		}
+	}
+	return false
+}
 
 func main() {
 	var input string
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Printf("Enter a String: ")
-	input, _ = reader.ReadString('\n')
-	input = strings.ToLower(input)
-	var length int = len(input);
 	
-	if input[0] != 'i'|| input[length-1] != 'n'{
-		fmt.Printf("Not Found")
+	fmt.Printf("Enter a String: ")
+	val, err := fmt.Scanln(&input)
+	
+	if err != nil{
+		fmt.Printf("[ERROR]: Cannot parse your input: %s", err)
+	} else if val == 0 {
+		fmt.Printf("[ERROR]: please type your string")
 	} else {
-		for i:=1; i<length-1; i++{
-			if input[i] == 'a'{
-				fmt.Printf("Found")
-				return
-			}
+		input = strings.ToLower(input)
+		if IsPattern1(input){
+			fmt.Printf("Found")
+		} else {
+			fmt.Printf("Not Found")
 		}
-		fmt.Printf("Not Found")
 	}
 }
